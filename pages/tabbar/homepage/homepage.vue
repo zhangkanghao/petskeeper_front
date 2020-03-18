4a8eec2f46821388b4bf17bc6cd8acaeca9e27f4
koer <koerzhang@163.com>
2020-03-18 14:24:34 +0800

    首页导航栏

--- pages/tabbar/homepage/homepage.vue
+++ pages/tabbar/homepage/homepage.vue
@@ -5,24 +5,71 @@
 			<view class="top_view"></view>
 		</view>
 		<!-- #endif -->
-		<uni-nav-bar backgroundColor="#007AFF" color="#f8f8f8" right-text="注销" title="个人中心" ></uni-nav-bar>
-		页面 - 1
+		<view class="example-body">
+			<uni-nav-bar :fixed="false" color="#333333" background-color="#FFFFFF" right-icon="scan" @clickLeft="showCity"
+			 @clickRight="scan">
+				<block slot="left">
+					<view class="city">
+						<view><text class="uni-nav-bar-text">{{ city }}</text></view>
+						<uni-icons type="arrowdown" color="#333333" size="22" />
+					</view>
+				</block>
+				<view class="input-view">
+					<uni-icons class="input-uni-icon" type="search" size="22" color="#666666" />
+					<input confirm-type="search" class="nav-bar-input" type="text" placeholder="输入搜索关键词" @confirm="confirm">
+				</view>
+			</uni-nav-bar>
+		</view>
+		<view id="scroll" class="scrollView">
+			<scroll-view class="scroll-view_H" scroll-x="true" @scroll="scroll" scroll-left="120">
+				<view id="demo1" class="scroll-view-item_H">A</view>
+				<view id="demo2" class="scroll-view-item_H">B</view>
+				<view id="demo3" class="scroll-view-item_H">C</view>
+			</scroll-view>
+		</view>
+		<view id="waterfull" class="waterfall">
+
+		</view>
 	</view>
 </template>
 
 <script>
+	import uniIcons from '@/components/uni-icons/uni-icons.vue'
 	import uniNavBar from "@/components/uni-nav-bar/uni-nav-bar.vue"
 	export default {
 		components: {
+			uniIcons,
 			uniNavBar
 		},
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
+				city: '北京'
 			};
 		},
 
-		methods: {}
+		methods: {
+			showCity() {
+
+				uni.showToast({
+					title: '选择城市'
+				})
+			},
+			scan() {
+				uni.showToast({
+					title: '扫码'
+				})
+			},
+			confirm() {
+				uni.showToast({
+					title: '搜索'
+				})
+			},
+			scroll: function(e) {
+				console.log(e)
+				this.old.scrollTop = e.detail.scrollTop
+			},
+		}
 	};
 </script>
 
@@ -30,20 +77,98 @@
 	.status_bar {
 		height: var(--status-bar-height);
 		width: 100%;
-		background-color: #007AFF;
+		background-color: #ffffff;
 	}
 
 	.top_view {
 		height: var(--status-bar-height);
 		width: 100%;
 		position: fixed;
-		background-color: #007AFF;
+		background-color: #ffffff;
 		top: 0;
 		z-index: 999;
 	}
 
+	.example-body {
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: center;
+		padding: 0;
+		font-size: 14px;
+		background-color: #ffffff;
+	}
+
+	.uni-nav-bar-text {
+		font-size: 28rpx;
+	}
+
+	.city {
+		/* #ifndef APP-PLUS-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		align-items: center;
+		justify-content: flex-start;
+		/* width: 160rpx;
+	*/
+		margin-left: 4px;
+	}
+
+	.input-view {
+		/* #ifndef APP-PLUS-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		/* width: 500rpx;*/
+		flex: 1;
+		background-color: #f8f8f8;
+		height: 30px;
+		border-radius: 15px;
+		padding: 0 15px;
+		flex-wrap: nowrap;
+		margin: 7px 0;
+		line-height: 30px;
+	}
+
+	.input-uni-icon {
+		line-height: 30px;
+	}
+
+	.nav-bar-input {
+		height: 30px;
+		line-height: 30px;
+		/* #ifdef APP-PLUS-NVUE */
+		width: 370rpx;
+		/* #endif */
+		padding: 0 5px;
+		font-size: 28rpx;
+		background-color: #f8f8f8;
+		text-align: left;
+	}
+
+
 	.content {
 		text-align: center;
 		height: 100%;
+	}
+
+	.scrollView {
+		width: 100%;
+		height: 280upx;
+		background-color: #FFCC01;
+	}
+	.scroll-view_H{
+		width: 100%;
+		height: 100%;
+		display: flex;
+	}
+	.scroll-view-item_H{
+		
+	}
+	.waterfall {
+		height: 470upx;
+		width: 100%;
+		margin: 5upx auto;
+		background-color: #007AFF;
 	}
 </style>
