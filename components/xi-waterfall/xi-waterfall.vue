a2e500ee158878375cba9b02a7b15ecc5d8d2a7e
koer <koerzhang@163.com>
2020-03-18 17:05:05 +0800

    美化瀑布流样式

--- components/xi-waterfall/xi-waterfall.vue
+++ components/xi-waterfall/xi-waterfall.vue
@@ -2,133 +2,235 @@
 	<view class="container">
 		<!-- 第一列 -->
 		<view class="column" id="columnFirst">
-			<view class="item" v-for="(item, index) in columnFirst" :key="index">
-				<image :src="item.img" mode="widthFix"></image>
-				<view>{{ item.title }}</view>
+			<view class="water-item" v-for="(item,index) in columnFirst" :key="index">
+				<view>
+					<image class="top-cover" :src="item.img" :onerror="errorImg" mode="widthFix"></image>
+					<h3 class="top-title">{{item.title}}</h3>
+				</view>
+				<view class="water-bottom">
+					<view class="water-bottom-item">
+						<view class="img-box">
+							<image class="water-avatar" :src="item.photo" mode="widthFix"></image>
+						</view>
+						<text class="water-name">{{item.name}}</text>
+					</view>
+					<view class="water-bottom-item">
+						<image class="bottom-good" :onerror="errorImg" src="/static/icon_good.svg" mode="widthFix"></image>
+						<text class="water-num">{{item.likeCount}}</text>
+					</view>
+				</view>
+				<!-- 	<image :src="item.img" mode="widthFix"></image>
+				<view>{{ item.title }}</view> -->
 			</view>
 		</view>
 		<!-- 第二列 -->
 		<view class="column" id="columnSecond">
-			<view class="item" v-for="(item, index) in columnSecond" :key="index">
-				<image :src="item.img" mode="widthFix"></image>
-				<view>{{ item.title }}</view>
+			<view class="water-item" v-for="(item,index) in columnSecond" :key="index">
+				<view>
+					<image class="top-cover" :src="item.img" :onerror="errorImg" mode="widthFix"></image>
+					<h3 class="top-title">{{item.title}}</h3>
+				</view>
+				<view class="water-bottom">
+					<view class="water-bottom-item">
+						<view class="img-box">
+							<image class="water-avatar" :src="item.photo" mode="widthFix"></image>
+						</view>
+						<text class="water-name">{{item.name}}</text>
+					</view>
+					<view class="water-bottom-item">
+						<image class="bottom-good" :onerror="errorImg" src="/static/icon_good.svg" mode="widthFix"></image>
+						<text class="water-num">{{item.likeCount}}</text>
+					</view>
+				</view>
+				<!-- 	<image :src="item.img" mode="widthFix"></image>
+					<view>{{ item.title }}</view> -->
 			</view>
 		</view>
 	</view>
 </template>
 
 <script>
-export default {
-	props: {
-		list: {
-			type: Array,
-			default: function() {
-				return [];
-			}
-		}
-	},
-	data() {
-		return {
-			tempArray: [],
-			columnFirst: [],
-			columnSecond: [],
-			columnFirstHeight: 0,
-			columnSecondHeight: 0,
-			switchInsert: true
-		};
-	},
-	methods: {
-		// 获取dom元素的高度
-		async getDomHeight(selector) {
-			return new Promise(resolve => {
-				let query = uni.createSelectorQuery().in(this);
-				query
-					.select(selector)
-					.fields(
-						{
-							size: true
-						},
-						data => {
-							resolve(data.height);
-						}
-					)
-					.exec();
-			});
-		},
-		//新增数据
-		async insert() {
-			//只有数组内有数据才执行
-			if (this.tempArray.length > 0) {
-				this.switchInsert = false;
-				//获取每一列的高度
-				let columnFirstHeight = await this.getDomHeight('#columnFirst');
-				let columnSecondHeight = await this.getDomHeight('#columnSecond');
-				//获取加入的数据
-				let data = this.tempArray.splice(0, 1);
-				if (columnFirstHeight == columnSecondHeight || columnFirstHeight < columnSecondHeight) {
-					//当高度相对,或第一列小于其他列,数据加入到第一列
-					this.columnFirst.push(data[0]);
-				} else {
-					//数据加入到第二列
-					this.columnSecond.push(data[0]);
+	export default {
+		props: {
+			list: {
+				type: Array,
+				default: function() {
+					return [];
 				}
-				this.$nextTick(() => {
-					//当数据渲染完毕后,可以进行添加,知道临时数据被添加完毕
-					this.insert();
+			}
+		},
+		data() {
+			return {
+				tempArray: [],
+				columnFirst: [],
+				columnSecond: [],
+				columnFirstHeight: 0,
+				columnSecondHeight: 0,
+				switchInsert: true
+			};
+		},
+		methods: {
+			// 获取dom元素的高度
+			async getDomHeight(selector) {
+				return new Promise(resolve => {
+					let query = uni.createSelectorQuery().in(this);
+					query
+						.select(selector)
+						.fields({
+								size: true
+							},
+							data => {
+								resolve(data.height);
+							}
+						)
+						.exec();
 				});
-			} else {
-				this.switchInsert = true;
-			}
-		},
-		//清除数组
-		clear() {
-			this.columnFirst = [];
-			this.columnSecond = [];
-			this.tempArray = [];
-		},
-		//外部调用,添加数据
-		insertData(data) {
-			this.tempArray = this.tempArray.concat(data);
+			},
 			//新增数据
-			if (this.switchInsert) {
-				this.insert();
-			}
-		}
-	},
-	watch: {
-		list(newVal, oldVal) {
-			//当监听到,数据有变化时
-			if (newVal.length === 0) {
-				//清空数据
-				this.clear();
-			} else if (newVal.length > 0) {
-				//赋值给临时数组
-				this.tempArray = this.tempArray.concat(newVal.slice(oldVal.length));
+			async insert() {
+				//只有数组内有数据才执行
+				if (this.tempArray.length > 0) {
+					this.switchInsert = false;
+					//获取每一列的高度
+					let columnFirstHeight = await this.getDomHeight('#columnFirst');
+					let columnSecondHeight = await this.getDomHeight('#columnSecond');
+					//获取加入的数据
+					let data = this.tempArray.splice(0, 1);
+					if (columnFirstHeight == columnSecondHeight || columnFirstHeight < columnSecondHeight) {
+						//当高度相对,或第一列小于其他列,数据加入到第一列
+						this.columnFirst.push(data[0]);
+					} else {
+						//数据加入到第二列
+						this.columnSecond.push(data[0]);
+					}
+					this.$nextTick(() => {
+						//当数据渲染完毕后,可以进行添加,知道临时数据被添加完毕
+						this.insert();
+					});
+				} else {
+					this.switchInsert = true;
+				}
+			},
+			//清除数组
+			clear() {
+				this.columnFirst = [];
+				this.columnSecond = [];
+				this.tempArray = [];
+			},
+			//外部调用,添加数据
+			insertData(data) {
+				this.tempArray = this.tempArray.concat(data);
 				//新增数据
 				if (this.switchInsert) {
 					this.insert();
 				}
 			}
+		},
+		watch: {
+			list(newVal, oldVal) {
+				//当监听到,数据有变化时
+				if (newVal.length === 0) {
+					//清空数据
+					this.clear();
+				} else if (newVal.length > 0) {
+					//赋值给临时数组
+					this.tempArray = this.tempArray.concat(newVal.slice(oldVal.length));
+					//新增数据
+					if (this.switchInsert) {
+						this.insert();
+					}
+				}
+			}
+		},
+		computed: {
+			currentLength() {
+				return this.columnFirst.length + this.columnSecond.length;
+			}
 		}
-	},
-	computed: {
-		currentLength() {
-			return this.columnFirst.length + this.columnSecond.length;
-		}
-	}
-};
+	};
 </script>
 
 <style>
-.container {
-	width: 100%;
-	display: flex;
-	align-items: flex-start;
-}
-.container .column {
-	flex: 1;
-}
-.container .column .item image {
-	width: 100%;
-}
+	.container {
+		width: 100%;
+		display: flex;
+		align-items: flex-start;
+	}
+
+	.container .column {
+		flex: 1;
+	}
+
+	.container .column .item image {
+		width: 100%;
+	}
+
+	.water-item {
+		width: 97%;
+		margin: 0 auto;
+		border-radius: 12upx;
+		overflow: hidden;
+		background-color: #FFFFFF;
+		margin-bottom: 10upx;
+	}
+
+	.top-cover {
+		width: 100%;
+	}
+
+	.top-title {
+		font-family: PingFangHK-Medium;
+		font-size: 28upx;
+		font-weight: 600;
+		line-height: 40upx;
+		color: #333333;
+		text-align: justify;
+		margin: 20upx;
+	}
+
+	.water-bottom {
+		display: flex;
+		justify-content: space-between;
+		margin: 0 20upx 20upx 20upx;
+		font-size: 24upx;
+	}
+
+	.water-bottom-item {
+		display: flex;
+		align-items: center;
+	}
+
+	.img-box {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 50upx;
+		height: 50upx;
+		border-radius: 50%;
+		overflow: hidden;
+	}
+
+	.water-avatar {
+		width: 50upx;
+	}
+
+	.water-name {
+		max-width: 160upx;
+		padding-left: 10upx;
+		overflow: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+	}
+
+	.water-num {
+		line-height: 32upx;
+		font-size: 24upx;
+		color: #333;
+		margin-left: 14upx;
+	}
+
+	.bottom-good {
+		width: 28upx;
+		height: 28upx;
+	}
 </style>
--- pages/tabbar/homepage/homepage.vue
+++ pages/tabbar/homepage/homepage.vue
@@ -44,7 +44,7 @@
 				<waterfall :list="list"></waterfall>
 			</view>
 		</scroll-view>
-		
+
 	</view>
 </template>
 
@@ -68,84 +68,185 @@
 					scrollTop: 0
 				},
 				list: [],
-				data: [
-					{
-						title: '标题1',
-						img: 'https://s2.ax1x.com/2020/02/28/3Dn6c8.png'
-					},
-					{
-						title: '标题2',
-						img: 'https://s2.ax1x.com/2020/02/28/3DntXD.jpg'
-					},
-					{
-						title: '标题3',
-						img: 'https://s2.ax1x.com/2020/02/28/3Dna0H.jpg'
-					},
-					{
-						title: '标题4',
-						img: 'https://s2.ax1x.com/2020/02/28/3Dn0AA.jpg'
-					},
-					{
-						title: '标题5',
-						img: 'https://s2.ax1x.com/2020/02/28/3DnRBQ.jpg'
-					},
-					{
-						title: '标题6',
-						img: 'https://s2.ax1x.com/2020/02/28/3Dns9P.jpg'
-					},
-					{
-						title: '标题7',
-						img: 'https://s2.ax1x.com/2020/02/28/3DncjS.jpg'
-					},
-					{
-						title: '标题8',
-						img: 'https://s2.ax1x.com/2020/02/28/3Dn2ng.jpg'
-					},
-					{
-						title: '标题2',
-						img: 'https://s2.ax1x.com/2020/02/28/3DntXD.jpg'
-					},
-					{
-						title: '标题3',
-						img: 'https://s2.ax1x.com/2020/02/28/3Dna0H.jpg'
-					},
-					{
-						title: '标题4',
-						img: 'https://s2.ax1x.com/2020/02/28/3Dn0AA.jpg'
-					},
-					{
-						title: '标题5',
-						img: 'https://s2.ax1x.com/2020/02/28/3DnRBQ.jpg'
-					},
-					{
-						title: '标题6',
-						img: 'https://s2.ax1x.com/2020/02/28/3Dns9P.jpg'
-					},
-					{
-						title: '标题7',
-						img: 'https://s2.ax1x.com/2020/02/28/3DncjS.jpg'
-					},
-					{
-						title: '标题8',
-						img: 'https://s2.ax1x.com/2020/02/28/3Dn2ng.jpg'
-					}
-				]
+				data: [{
+					img: "http://pic1.win4000.com/wallpaper/2018-03-28/5abafe0c311e6.jpg",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 0,
+					name: "笑饮孤鸿",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173486008&di=68514e7684d0753c5100994fae6456cb&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201612%2F07%2F20161207195613_xuEFP.thumb.700_0.jpeg",
+					title: "虽然你我会下落不明",
+				}, {
+					img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173237976&di=4026f6a26b2c370611d6c6877aad1ded&imgtype=0&src=http%3A%2F%2Fdmimg.5054399.com%2Fallimg%2Fqidai%2Fndmzsjbz%2F001.jpg",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 520,
+					name: "残城碎梦",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173486004&di=3ef63a12a8372b81bfe97b81adee093d&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201612%2F07%2F20161207195604_jNSGc.thumb.700_0.jpeg",
+					title: "你知道我曾为你动过情"
+				}, {
+					img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173237976&di=4026f6a26b2c370611d6c6877aad1ded&imgtype=0&src=http%3A%2F%2Fdmimg.5054399.com%2Fallimg%2Fqidai%2Fndmzsjbz%2F001.jpg",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 650,
+					name: "梦里南柯",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173486004&di=3ef63a12a8372b81bfe97b81adee093d&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201612%2F07%2F20161207195604_jNSGc.thumb.700_0.jpeg",
+					title: "我欲与君相知，长命无绝衰。山无陵，江水为竭。东雷震震，夏雨雪。天地合，乃敢与君绝"
+				}, {
+					img: "http://img.51rry.com/note/2019-10-21/1571654591036.png",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 999,
+					name: "无风",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173620777&di=54b7ee5f59c30503096ccabbe0ed9a8e&imgtype=0&src=http%3A%2F%2Fp.store.itangyuan.com%2Fp%2Fchapter%2Fattachment%2Feg-VEBjtEA%2FEgfWEgMVEgjTe_-W4BEU40u3KhifeUL1HtDvHhsVgU9hK6LqG1opiMO.jpg",
+					title: "晓看天色，暮看云，行也思君，坐也思君",
+				}, {
+					img: "http://pic1.win4000.com/wallpaper/2018-10-31/5bd99d277748e.jpg",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 25,
+					name: "君倾心",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173486008&di=68514e7684d0753c5100994fae6456cb&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201612%2F07%2F20161207195613_xuEFP.thumb.700_0.jpeg",
+					title: "众里寻她千百度，蓦然回首，那人却在，灯火阑珊处",
+				}, {
+					img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173155133&di=9d77167bfd27f6937ffbccbd3eddb71e&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201601%2F09%2F20160109201510_fucHC.jpeg",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 4,
+					name: "青丝弄瑶玉",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173620777&di=54b7ee5f59c30503096ccabbe0ed9a8e&imgtype=0&src=http%3A%2F%2Fp.store.itangyuan.com%2Fp%2Fchapter%2Fattachment%2Feg-VEBjtEA%2FEgfWEgMVEgjTe_-W4BEU40u3KhifeUL1HtDvHhsVgU9hK6LqG1opiMO.jpg",
+					title: "死生契阔，与子成说。执子之手，与子偕老",
+				}, {
+					img: "http://img.51rry.com/note/2019-11-28/1574931683000.png",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 520,
+					name: "残城碎梦",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173486004&di=3ef63a12a8372b81bfe97b81adee093d&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201612%2F07%2F20161207195604_jNSGc.thumb.700_0.jpeg",
+					title: "你知道我曾为你动过情"
+				}, {
+					img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173237976&di=4026f6a26b2c370611d6c6877aad1ded&imgtype=0&src=http%3A%2F%2Fdmimg.5054399.com%2Fallimg%2Fqidai%2Fndmzsjbz%2F001.jpg",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 650,
+					name: "梦里南柯",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173486004&di=3ef63a12a8372b81bfe97b81adee093d&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201612%2F07%2F20161207195604_jNSGc.thumb.700_0.jpeg",
+					title: "我欲与君相知，长命无绝衰。山无陵，江水为竭。东雷震震，夏雨雪。天地合，乃敢与君绝"
+				}, {
+					img: "http://img.51rry.com/note/2019-10-21/1571654591036.png",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 999,
+					name: "无风",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173620777&di=54b7ee5f59c30503096ccabbe0ed9a8e&imgtype=0&src=http%3A%2F%2Fp.store.itangyuan.com%2Fp%2Fchapter%2Fattachment%2Feg-VEBjtEA%2FEgfWEgMVEgjTe_-W4BEU40u3KhifeUL1HtDvHhsVgU9hK6LqG1opiMO.jpg",
+					title: "晓看天色，暮看云，行也思君，坐也思君",
+				}, {
+					img: "http://pic1.win4000.com/wallpaper/2018-10-31/5bd99d277748e.jpg",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 25,
+					name: "君倾心",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173486008&di=68514e7684d0753c5100994fae6456cb&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201612%2F07%2F20161207195613_xuEFP.thumb.700_0.jpeg",
+					title: "众里寻她千百度，蓦然回首，那人却在，灯火阑珊处",
+				}, {
+					img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173155133&di=9d77167bfd27f6937ffbccbd3eddb71e&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201601%2F09%2F20160109201510_fucHC.jpeg",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 4,
+					name: "青丝弄瑶玉",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173620777&di=54b7ee5f59c30503096ccabbe0ed9a8e&imgtype=0&src=http%3A%2F%2Fp.store.itangyuan.com%2Fp%2Fchapter%2Fattachment%2Feg-VEBjtEA%2FEgfWEgMVEgjTe_-W4BEU40u3KhifeUL1HtDvHhsVgU9hK6LqG1opiMO.jpg",
+					title: "死生契阔，与子成说。执子之手，与子偕老",
+				}, {
+					img: "http://img.51rry.com/note/2019-11-28/1574931683000.png",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 520,
+					name: "残城碎梦",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173486004&di=3ef63a12a8372b81bfe97b81adee093d&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201612%2F07%2F20161207195604_jNSGc.thumb.700_0.jpeg",
+					title: "你知道我曾为你动过情"
+				}, {
+					img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173237976&di=4026f6a26b2c370611d6c6877aad1ded&imgtype=0&src=http%3A%2F%2Fdmimg.5054399.com%2Fallimg%2Fqidai%2Fndmzsjbz%2F001.jpg",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 650,
+					name: "梦里南柯",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173486004&di=3ef63a12a8372b81bfe97b81adee093d&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201612%2F07%2F20161207195604_jNSGc.thumb.700_0.jpeg",
+					title: "我欲与君相知，长命无绝衰。山无陵，江水为竭。东雷震震，夏雨雪。天地合，乃敢与君绝"
+				}, {
+					img: "http://img.51rry.com/note/2019-10-21/1571654591036.png",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 999,
+					name: "无风",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173620777&di=54b7ee5f59c30503096ccabbe0ed9a8e&imgtype=0&src=http%3A%2F%2Fp.store.itangyuan.com%2Fp%2Fchapter%2Fattachment%2Feg-VEBjtEA%2FEgfWEgMVEgjTe_-W4BEU40u3KhifeUL1HtDvHhsVgU9hK6LqG1opiMO.jpg",
+					title: "晓看天色，暮看云，行也思君，坐也思君",
+				}, {
+					img: "http://pic1.win4000.com/wallpaper/2018-10-31/5bd99d277748e.jpg",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 25,
+					name: "君倾心",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173486008&di=68514e7684d0753c5100994fae6456cb&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201612%2F07%2F20161207195613_xuEFP.thumb.700_0.jpeg",
+					title: "众里寻她千百度，蓦然回首，那人却在，灯火阑珊处",
+				}, {
+					img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173155133&di=9d77167bfd27f6937ffbccbd3eddb71e&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201601%2F09%2F20160109201510_fucHC.jpeg",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 4,
+					name: "青丝弄瑶玉",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173620777&di=54b7ee5f59c30503096ccabbe0ed9a8e&imgtype=0&src=http%3A%2F%2Fp.store.itangyuan.com%2Fp%2Fchapter%2Fattachment%2Feg-VEBjtEA%2FEgfWEgMVEgjTe_-W4BEU40u3KhifeUL1HtDvHhsVgU9hK6LqG1opiMO.jpg",
+					title: "死生契阔，与子成说。执子之手，与子偕老",
+				}, {
+					img: "http://img.51rry.com/note/2019-11-28/1574931683000.png",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 520,
+					name: "残城碎梦",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173486004&di=3ef63a12a8372b81bfe97b81adee093d&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201612%2F07%2F20161207195604_jNSGc.thumb.700_0.jpeg",
+					title: "你知道我曾为你动过情"
+				}, {
+					img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173237976&di=4026f6a26b2c370611d6c6877aad1ded&imgtype=0&src=http%3A%2F%2Fdmimg.5054399.com%2Fallimg%2Fqidai%2Fndmzsjbz%2F001.jpg",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 650,
+					name: "梦里南柯",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173486004&di=3ef63a12a8372b81bfe97b81adee093d&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201612%2F07%2F20161207195604_jNSGc.thumb.700_0.jpeg",
+					title: "我欲与君相知，长命无绝衰。山无陵，江水为竭。东雷震震，夏雨雪。天地合，乃敢与君绝"
+				}, {
+					img: "http://img.51rry.com/note/2019-10-21/1571654591036.png",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 999,
+					name: "无风",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173620777&di=54b7ee5f59c30503096ccabbe0ed9a8e&imgtype=0&src=http%3A%2F%2Fp.store.itangyuan.com%2Fp%2Fchapter%2Fattachment%2Feg-VEBjtEA%2FEgfWEgMVEgjTe_-W4BEU40u3KhifeUL1HtDvHhsVgU9hK6LqG1opiMO.jpg",
+					title: "晓看天色，暮看云，行也思君，坐也思君",
+				}, {
+					img: "http://pic1.win4000.com/wallpaper/2018-10-31/5bd99d277748e.jpg",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 25,
+					name: "君倾心",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173486008&di=68514e7684d0753c5100994fae6456cb&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201612%2F07%2F20161207195613_xuEFP.thumb.700_0.jpeg",
+					title: "众里寻她千百度，蓦然回首，那人却在，灯火阑珊处",
+				}, {
+					img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173155133&di=9d77167bfd27f6937ffbccbd3eddb71e&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201601%2F09%2F20160109201510_fucHC.jpeg",
+					id: "1084",
+					isLiked: "0",
+					likeCount: 4,
+					name: "青丝弄瑶玉",
+					photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173620777&di=54b7ee5f59c30503096ccabbe0ed9a8e&imgtype=0&src=http%3A%2F%2Fp.store.itangyuan.com%2Fp%2Fchapter%2Fattachment%2Feg-VEBjtEA%2FEgfWEgMVEgjTe_-W4BEU40u3KhifeUL1HtDvHhsVgU9hK6LqG1opiMO.jpg",
+					title: "死生契阔，与子成说。执子之手，与子偕老",
+				}]
 			};
 		},
 		onLoad() {
-			console.log(this.list);
-			_self=this;
-		},
-		onShow() {
-			console.log('home show');
-			setTimeout(function(){
-				_self.list=_self.list.concat(_self.data);
-				console.log(_self.list);
-			},100)
+			_self = this;
+			setTimeout(function() {
+				_self.list = _self.list.concat(_self.data);
+			}, 100)
 		},
 		methods: {
 			showCity() {
-
 				uni.showToast({
 					title: '选择城市'
 				})
@@ -171,6 +272,7 @@
 	.content {
 		text-align: center;
 		height: 100%;
+		background-color: #eeeeee;
 	}
 
 	.status_bar {
@@ -251,11 +353,12 @@
 		background-color: #FFCC01;
 	}
 
-	.swiper-item{
+	.swiper-item {
 		width: 100%;
 		height: 280upx;
 	}
-	.swiper-item>image{
+
+	.swiper-item>image {
 		width: 100%;
 		height: 280upx;
 	}
