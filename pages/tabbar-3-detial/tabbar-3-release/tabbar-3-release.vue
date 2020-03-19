efd0b179a2efef2e12a620e0dd791d1e9a021270
koer <koerzhang@163.com>
2020-03-19 14:25:59 +0800

    发图文页面完成 发视频和问答一个框架

--- pages/tabbar-3-detial/tabbar-3-release/tabbar-3-release.vue
+++ pages/tabbar-3-detial/tabbar-3-release/tabbar-3-release.vue
@@ -1,6 +1,45 @@
 <template>
 	<view class="content">
-		页面 - 发图文
+		<!-- #ifdef APP-PLUS -->
+		<view class="status_bar">
+			<view class="top_view"></view>
+		</view>
+		<!-- #endif -->
+		<view class="example-body" style="border-bottom: #f1f1f1 3upx solid;">
+			<uni-nav-bar :fixed="true" color="#333333"  leftIcon="left-nav" leftText="取消" title="添加动态" rightText="发布" @clickLeft="cancel">
+				<block class="cu-item" slot="right">
+					<text class="lg text-black cuIcon-camerafill" style="font-size: 40upx;margin-left: 10upx;"></text>
+				</block>
+			</uni-nav-bar>
+		</view>
+		<form>
+			<view class="cu-form-group margin-top align-start" style="height: 300upx;">
+				<textarea class="tatext" maxlength="-1" @input="textareaAInput" placeholder="多行文本输入框"></textarea>
+			</view>
+			<!-- 图片 -->
+			<view class="cu-form-group">
+				<view class="grid col-3 grid-square flex-sub">
+					<view class="bg-img" v-for="(item,index) in imgList" :key="index" @tap="ViewImage" :data-url="imgList[index]">
+						<image class="image" :src="imgList[index]" mode="aspectFill"></image>
+						<view class="cu-tag bg-red" @tap.stop="DelImg" :data-index="index">
+							<text class='cuIcon-close'></text>
+						</view>
+					</view>
+					<view class="solids" @tap="ChooseImage" v-if="imgList.length<9">
+						<text class='cuIcon-cameraadd'></text>
+					</view>
+				</view>
+			</view>
+			<view class="cu-form-group margin-top">
+				<view class="title">所有人可见</view>
+				<switch @change="SwitchA" :class="switchA?'checked':''" :checked="switchA?true:false"></switch>
+			</view>
+			<view class="cu-form-group margin-top">
+				<view class="title">匿名</view>
+				<switch @change="Annoymous" :class="annoymous?'checked':''" :checked="annoymous?true:false"></switch>
+			</view>
+			
+		</form>
 	</view>
 </template>
 
@@ -8,22 +47,115 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
+				textareaAValue:'',
+				index: -1,
+				imgList: [],
+				switchA:true,
+				annoymous:false,
+				status:0,
+				region: ['广东省', '广州市', '海珠区'],
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			cancel(){
+				if(this.textareaAValue!=''){
+					uni.showModal({
+						title:'提示',
+						confirmText:'保存',
+						cancelText:'拒绝',
+						content:'内容将被清空，是否保存为草稿',
+						success:res => {
+							if(res.confirm){
+								console.log('上传草稿');
+								status=1;
+								// uni.request({
+								// 	url: '',
+								// 	method: 'GET',
+								// 	data: {},
+								// 	success: res => {},
+								// 	fail: () => {},
+								// 	complete: () => {}
+								// });
+							}
+							uni.navigateBack({
+								delta:1,
+								animationType:'slide-out-bottom'
+							})
+						}
+					})
+				}else{
+					uni.navigateBack({
+						delta:1,
+						animationType:'slide-out-bottom'
+					})
+				}
+				
+			},
+			textareaAInput(e) {
+				this.textareaAValue = e.detail.value
+			},
+			ViewImage(e) {
+				uni.previewImage({
+					urls: this.imgList,
+					current: e.currentTarget.dataset.url
+				});
+			},
+			DelImg(e) {
+				this.imgList.splice(e.currentTarget.dataset.index, 1)
+			},	
+			ChooseImage() {
+				console.log();
+				uni.chooseImage({
+					count: 9-this.imgList.length, //默认9
+					sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+					sourceType: ['album'], //从相册选择
+					success: (res) => {
+						if (this.imgList.length != 0) {
+							this.imgList = this.imgList.concat(res.tempFilePaths)
+						} else {
+							this.imgList = res.tempFilePaths
+						}
+					}
+				});
+			},
+			SwitchA(e) {
+				this.switchA = e.detail.value
+			},
+			Annoymous(e){
+				this.annoymous=e.detail.value
+			}
 		}
 	}
 </script>
 
 <style>
+	page{
+		background-color: #FFFFFF;
+	}
+	.status_bar {
+		height: var(--status-bar-height);
+		width: 100%;
+		background-color: #ffffff;
+	}
+	
+	.top_view {
+		height: var(--status-bar-height);
+		width: 100%;
+		position: fixed;
+		background-color: #ffffff;
+		top: 0;
+		z-index: 999;
+	}
 	.content {
-		text-align: center;
-		height: 400upx;
-		margin-top: 200upx;
+		height: 100%;
+		background-color: #FFFFFF;
+	}
+	.tatext{
+		text-align: start;
+		height: 500upx;
 	}
 </style>
--- pages/tabbar-3-detial/tabbar-3-video/tabbar-3-video.vue
+++ pages/tabbar-3-detial/tabbar-3-video/tabbar-3-video.vue
@@ -1,6 +1,17 @@
 <template>
 	<view class="content">
-		页面 - 发视频
+		<!-- #ifdef APP-PLUS -->
+		<view class="status_bar">
+			<view class="top_view"></view>
+		</view>
+		<!-- #endif -->
+		<view class="example-body" style="border-bottom: #f1f1f1 3upx solid;">
+			<uni-nav-bar :fixed="true" color="#333333"  leftIcon="left-nav" leftText="取消" title="添加视频" rightText="发布" @clickLeft="cancel">
+				<block class="cu-item" slot="right">
+					<text class="lg text-black cuIcon-camerafill" style="font-size: 40upx;margin-left: 10upx;"></text>
+				</block>
+			</uni-nav-bar>
+		</view>
 	</view>
 </template>
 
@@ -15,15 +26,38 @@
 
 		},
 		methods: {
-
+			cancel:()=>{
+				uni.navigateBack({
+					delta:1,
+					animationType:'slide-out-bottom'
+				})
+			}
+				
 		}
 	}
 </script>
 
 <style>
+	page{
+		background-color: #FFFFFF;
+	}
+	.status_bar {
+		height: var(--status-bar-height);
+		width: 100%;
+		background-color: #ffffff;
+	}
+	
+	.top_view {
+		height: var(--status-bar-height);
+		width: 100%;
+		position: fixed;
+		background-color: #ffffff;
+		top: 0;
+		z-index: 999;
+	}
 	.content {
 		text-align: center;
-		height: 400upx;
-		margin-top: 200upx;
+		height: 100%;
+		background-color: #FFFFFF;
 	}
 </style>
